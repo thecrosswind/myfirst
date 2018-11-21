41743c1f32fec98ae06156881c10089552b8c8a3
nibaba <863093755@qq.com>
2018-11-21 19:17:33 +0800

    index完成

--- js/common.js
+++ js/common.js
@@ -22,7 +22,46 @@ if (location.href.indexOf("login.html") === -1) {
     url: "/employee/checkRootLogin",
     // dataType: "json",
     success: function(res) {
-      console.log(res);
+      if (res.success) {
+        console.log("登陆了");
+      }
+
+      if (res.error === 400) {
+        // 进行拦截, 拦截到登录页
+        location.href = "login.html";
+      }
     }
   });
 }
+
+$(function() {
+  $(".category").on("click", function() {
+    $(this)
+      .next()
+      .stop()
+      .slideToggle();
+  });
+
+  $(".lt_topbar .icon_menu").click(function() {
+    $(".lt_aside").toggleClass("hidemenu");
+    $(".lt_topbar").toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+  });
+  //退出功能
+  $("#logoutBtn").click(function() {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      success: function(info) {
+        // console.log(info);
+        if (info.success) {
+          location.href = "login.html";
+        }
+      }
+    });
+  });
+  //模态框
+  $(".icon_logout").on("click", function() {
+    $("#modalLogout").modal("show");
+  });
+});
